77098695ef98d8fd6e0cb505a0a253cc7d5a4963
Sketch Company <mtb.2008@web.de>
2024-11-14 00:34:18 +0100

    0.0.1

--- studio.mjs
+++ studio.mjs
@@ -5,7 +5,7 @@ import NodeCache from "node-cache"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const chache = new NodeCache({stdTTL: 10, checkperiod: 1})
+const chache = new NodeCache({stdTTL: 15, checkperiod: 1})
 const BASE_DIR = __dirname + "/"
 const PROFILES_DIR = BASE_DIR + "/profiles/"
 
